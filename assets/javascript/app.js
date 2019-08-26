99a0ade31b07066dbd193e14034e7afd0f7810dc
dyl trimble <dyltrimble12@gmail.com>
2019-08-25 22:47:28 -0400

    added buttons with an array and gave them functionality as well as a change state function for the gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,34 @@ $(document).ready(function () {
 		for (i = 0; i < animals.length; i++) {
 			$("#animal-buttons").append("<button class='btn btn-success' data-animal='" + animals[i] + "'>" + animals[i] + "</button>");
 		}
+		$("button").on("click", function () {
+			var animalGif = $(this).attr("data-animal");
+			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+				animalGif + "&api_key=P78035KzeSFlvtBnDC9EoyNWvLAu6uV1"
+			changeState();
+			$.ajax({
+				url: queryURL,
+				method: "GET"
+			}).done(function (response) {
+				var results = response.data;
+				$("#animals").empty();
+				for (var i = 0; i < results.length; i++) {
+					var animalDiv = $("<div>");
+					var p = $("<p>").text("Rating: " + results[i].rating);
+					var animalImg = $("<img>");
+		
+					animalImg.attr("src", results[i].images.original_still.url);
+					animalImg.attr("data-still", results[i].images.original_still.url);
+					animalImg.attr("data-animate", results[i].images.original.url);
+					animalImg.attr("data-state", "still");
+					animalImg.attr("class", "gif");
+					animalDiv.append(p);
+					animalDiv.append(animalImg);
+					$("#animals").append(animalDiv);
+					console.log(animalGif)
+				}
+			});
+		});
 	}
 
 	renderButtons();
@@ -19,4 +47,21 @@ $(document).ready(function () {
 		renderButtons();
 	});
 
+	function changeState(){
+		var state = $(this).attr("data-state");
+		var animateImage = $(this).attr("data-animate");
+		var stillImage = $(this).attr("data-still");
+
+		if (state == "still") {
+			$(this).attr("src", animateImage);
+			$(this).attr("data-state", "animate");
+		}
+
+		else if (state == "animate") {
+			$(this).attr("src", stillImage);
+			$(this).attr("data-state", "still");
+		}
+	}
+	
+	$(document).on("click", ".gif", changeState);
 });
